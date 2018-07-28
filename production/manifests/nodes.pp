e4ec37b231386fc06fb494931b0f6f5826d2808c
Michal Musial <michal.musial@ing.com>
2018-07-28 17:38:29 +0200

    service ntpd enabled

--- production/manifests/nodes.pp
+++ production/manifests/nodes.pp
@@ -6,10 +6,18 @@ node 'wiki' {
   package { 'ntp':
     ensure  =>  'installed',
   }
+  service { 'ntpd':
+    ensure  =>  'runnig',
+    enabled =>  true,
+  }
 }
 
 node "wikitest" {
   package { 'ntp':
     ensure  =>  'installed',
+  }
+  service {'ntp':
+    ensure  =>  'running',
+    enabled =>  true,
   }
 }
